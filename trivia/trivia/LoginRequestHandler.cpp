ef1db047dafb706164ab97e70fd4adcda8bb69b1
1itswolf1 <144345852+1itswolf1@users.noreply.github.com>
2025-04-29 18:49:00 +0300

    need to fix simple bugs and check if works also comment

--- trivia/trivia/LoginRequestHandler.cpp
+++ trivia/trivia/LoginRequestHandler.cpp
@@ -50,22 +50,55 @@ RequestResult LoginRequestHandler::login(RequestInfo& request)
 	log = JsonRequestPacketDeserializer::deserializeLoginRequest(request.buffer);
 	std::string username = log.username;
 	std::string password = log.password;
-	if (LoginStatus::Success == this->m_handlerFactory.getLoginManager().login(username, password))
+	status = this->m_handlerFactory.getLoginManager().login(username, password);
+	if (status == LoginStatus::Success)
 	{
 		LoginResponse response{ LOGIN_CODE };
 		res.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
 		MenuRequestHandler* menuHandler = new MenuRequestHandler(); // Placeholder for the next handler – replace with actual logic late
-		res.newHandler = menuHandler;
+		res.newHandler = nullptr;
 	}
 	else
 	{
-		ErrorResponse error{ "Login failed" };
+		std::string errCode;
+		if(status == LoginStatus::UserNotExist) errCode = "UserNotExist";
+		else if(status == LoginStatus::WrongPassword) errCode = "WrongPassword"; // Placeholder for the next handler – replace with actual logic late ( sta)
+		else if(status == LoginStatus::AlreadyLoggedIn) errCode = "AlreadyLoggedIn"; // Placeholder for the next handler – replace with actual logic late ( s)
+		else if(status == LoginStatus::DbError) errCode = "DbError"; // Placeholder for the next handler – replace with actual logic late ( sta)
+		else { errCode = "login error"; }
+		ErrorResponse error{ errCode };
 		res.response = JsonResponsePacketSerializer::serializeErrorResponse(error);
+		res.newHandler = nullptr;
 	}
 	return res;
 }
 
 RequestResult LoginRequestHandler::signup(RequestInfo& request)
 {
-
+	SignupRequest log;
+	SignupStatus status;
+	RequestResult res;
+	log = JsonRequestPacketDeserializer::deserializeSignupRequest(request.buffer);
+	std::string username = log.username;
+	std::string password = log.password;
+	std::string email = log.email; // Add email to
+	status = this->m_handlerFactory.getLoginManager().signup(username, password, email);
+	if (status == SignupStatus::Success)
+	{
+		LoginResponse response{ SIGNUP_CODE };
+		res.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
+		MenuRequestHandler* menuHandler = new MenuRequestHandler(); // Placeholder for the next handler – replace with actual logic late
+		res.newHandler = nullptr;
+	}
+	else
+	{
+		std::string errCode;
+		if (status == SignupStatus::UserAlreadyExists) errCode = "UserAlreadyExists";
+		else if(status == SignupStatus::DbError) errCode = "DbErrors";
+		else { errCode = "Signup error"; }
+		ErrorResponse error{ errCode };
+		res.response = JsonResponsePacketSerializer::serializeErrorResponse(error);
+		res.newHandler = nullptr;
+	}
+	return res;
 }
